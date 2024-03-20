3f7414b07c86f30e94fc637f685144c60e870b5d
seul-dev <seul.dev@gmail.com>
2024-03-20 18:06:43 +0900

    add toast component props

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Button from '../Button';
 
 import styles from './ToastPlayground.module.css';
+import Toast from '../Toast/Toast';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
   const [message, setMessage] = React.useState('');
   const [variant, setVariant] = React.useState('notice');
+  const [isShow, setIsShow] = React.useState(false);
 
   return (
     <div className={styles.wrapper}>
@@ -17,6 +19,13 @@ function ToastPlayground() {
         <h1>Toast Playground</h1>
       </header>
 
+      <Toast
+        isShow={isShow}
+        variant={variant}
+        handleDismiss={() => setIsShow(false)}
+      >
+        {message}
+      </Toast>
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
           <label
@@ -58,7 +67,7 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button onClick={() => setIsShow(true)}>Pop Toast!</Button>
           </div>
         </div>
       </div>
